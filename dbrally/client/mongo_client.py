fe3ee0c83aefaa35e420854acdccf155409719e6
keenlok.lai <keenlok.lai@grabtaxi.com>
2024-07-30 17:02:10 +0800

    where to get query

--- dbrally/client/mongo_client.py
+++ dbrally/client/mongo_client.py
@@ -17,7 +17,7 @@ def get_conninfo(hosts, kwargs):
     host_str = ",".join([host.get('host') for host in hosts])
     user_spec = kwargs.get('userspec', '')
     url_prefix = hosts[0].get('url_prefix', '/')
-    url_query = hosts[0].get('url_query')
+    url_query = kwargs.get('url_query')
     conninfo = f"{scheme}://{user_spec}@{host_str}{url_prefix}{url_query}"
     print(conninfo)
     return conninfo
