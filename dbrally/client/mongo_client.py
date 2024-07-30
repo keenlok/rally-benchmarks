a134a4eb9fe168a6a2155938d31b9c7784a8b551
keenlok.lai <keenlok.lai@grabtaxi.com>
2024-07-30 13:11:42 +0800

    force srv

--- dbrally/client/mongo_client.py
+++ dbrally/client/mongo_client.py
@@ -13,7 +13,7 @@ def get_conninfo(hosts, kwargs):
         hosts = [{"host": "localhost"}]
 
     print(hosts, kwargs)
-    scheme = kwargs.get('scheme', 'mongo')
+    scheme = kwargs.get('scheme', 'mongo+srv')
     host_str = ",".join([host.get('host') for host in hosts])
     user_spec = kwargs.get('userspec', '')
     url_prefix = hosts[0].get('url_prefix', '/')
